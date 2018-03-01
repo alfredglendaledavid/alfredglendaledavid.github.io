59ac58f8dd7c0dd48d62a5dc5502a48b21e839bf
AlfredGOD27 <alfred.david@thehoneycombers.com>
2018-03-01 10:56:19 +0800

    inital commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,8 @@
 /*
-These functions make sure WordPress
-and Foundation play nice together.
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
-jQuery(document).ready(function() {
-	
-    // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
-
-	// Adds Flex Video to YouTube and Vimeo Embeds
-	jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
-		if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
-		  jQuery(this).wrap("<div class='widescreen responsive-embed'/>");
-		} else {
-		  jQuery(this).wrap("<div class='responsive-embed'/>");
-		}
-	});
-}); 
 
 (function($) {
 
